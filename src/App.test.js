800b507e1fb2b8d00a7ab78e624a78eb5359a5b1
edwards161 <acejakey@gmail.com>
2022-04-27 16:37:13 +0100

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//We can group tests with a describe block
+
+describe("app.js tests", () => {
+  test('renders to do header', () => {
+    render(<App />);
+    const linkElement = screen.getByText("TO DO LIST.");
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  test('renders to do h1 element', () => {
+    render(<App />);
+    const headElement = screen.getByRole("heading");
+    expect(headElement).toBeInTheDocument();
+  });
+  
+  test("Add to list button works", () => {
+    render(<App />)
+    const submitButton = screen.getByText("Add To List")
+    fireEvent.click(submitButton)
+    expect(deleteBtn).toBeInTheDocument()
+  });
+
 });
 
 
-test('To do enter bar is empty when add to list is clicked')
